64a548c08dce7d76fa727915c86da4f8e13267e6
shawnsandy <shawnsandy04@gmail.com>
2014-10-27 15:12:51 -0400

    Removed link to fopen-sans

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     <link rel="stylesheet" href="core-v3/fonts/open-sans/stylesheet.css"/>
     <link rel="stylesheet" href="core-v3/fonts/fontawesome/css/font-awesome.min.css"/>
     <link rel="stylesheet" href="adv_kiosks/style.css"/>
-    <link rel="stylesheet" href="fonts/open-sans/stylesheet.css"/>
+    <!-- <link rel="stylesheet" href="fonts/open-sans/stylesheet.css"/> -->
     <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
 
 </head>
